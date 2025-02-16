42a5b8f086aeac41a671d914c30cc96060cb3581
trunn2004 <trung.nq.64cntt@ntu.edu.vn>
2025-02-16 23:16:25 +0700

    Lab1_Bai1_DTB
    Bài 1: Nhập họ và tên + điểm trung bình. Xuất ra màn hình

---
+++ Lab1_Bai1_DTB/src/Bai1_DTB/DiemTrungBinh.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Bai1_DTB;
+
+import java.util.Scanner;
+/**
+ * 
+ */
+public class DiemTrungBinh {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập họ và tên:");
+		String hoTen = scanner.nextLine();
+		System.out.print("Nhập điểm trung bình:");
+		String diemTB = scanner.nextLine();
+		// TODO Auto-generated method stub
+
+	}
+
+}
