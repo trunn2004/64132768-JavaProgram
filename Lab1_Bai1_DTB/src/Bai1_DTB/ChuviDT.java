433d63003de473c92b67aace370855461ebbac8e
trunn2004 <trung.nq.64cntt@ntu.edu.vn>
2025-02-16 23:32:01 +0700

    Update ChuviDT.java

--- Lab1_Bai1_DTB/src/Bai1_DTB/ChuviDT.java
+++ Lab1_Bai1_DTB/src/Bai1_DTB/ChuviDT.java
@@ -2,7 +2,7 @@
  * 
  */
 package Bai1_DTB;
-
+import java.util.Scanner;
 /**
  * 
  */
@@ -12,7 +12,11 @@ public class ChuviDT {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập chiều dài của hình chữ nhật: ");
+        double chieuDai = scanner.nextDouble();
+        System.out.print("Nhập chiều rộng của hình chữ nhật: ");
+        double chieuRong = scanner.nextDouble();
 
 	}
 
