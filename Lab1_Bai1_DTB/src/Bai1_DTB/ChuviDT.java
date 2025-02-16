01f7dadf78fcbb465016e679e111866cb8f4f347
trunn2004 <trung.nq.64cntt@ntu.edu.vn>
2025-02-16 23:37:29 +0700

    Update ChuviDT.java

--- Lab1_Bai1_DTB/src/Bai1_DTB/ChuviDT.java
+++ Lab1_Bai1_DTB/src/Bai1_DTB/ChuviDT.java
@@ -16,8 +16,15 @@ public class ChuviDT {
         System.out.print("Nhập chiều dài của hình chữ nhật: ");
         double chieuDai = scanner.nextDouble();
         System.out.print("Nhập chiều rộng của hình chữ nhật: ");
+        
         double chieuRong = scanner.nextDouble();
-
+        double chuVi = 2 * (chieuDai + chieuRong);
+        double dienTich = chieuDai * chieuRong;
+        double canhNho = Math.min(chieuDai, chieuRong); //Math.min dùng để tìm giá trị nhỏ nhất trong hai số đó
+        
+        System.out.println("Chu vi của hình chữ nhật: " + chuVi);
+        System.out.println("Diện tích của hình chữ nhật: " + dienTich);
+        System.out.println("Cạnh nhỏ của hình chữ nhật: " + canhNho);
 	}
 
 }
