c1c55d473b4624b0b551f26b8be2cb73caab4287
trunn2004 <trung.nq.64cntt@ntu.edu.vn>
2025-02-16 23:23:27 +0700

    Lab1_Bai2_ChuviDT
    Bài 2: Tính chu vi, diện tích hình chữ nhật

---
+++ Lab1_Bai1_DTB/src/Bai1_DTB/ChuviDT.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Bai1_DTB;
+
+/**
+ * 
+ */
+public class ChuviDT {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
